3a106ab8551b557f65ac6ab649a99d89554a415f
Navdeep <abc>
2023-05-28 16:15:51 +0200

    Updated: 3 Files (AddressDetail)

---
+++ src/components/AddressDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="address-item"> 
+        {{ address.street + " " +address.number }}
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    address: {}
+  }
+}
+</script>
+
+<style scoped>
+.address-item {
+    color:green;
+    text-align: left;
+   
+
+
+}
+
+
+</style>
--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -2,23 +2,58 @@
   <div>
     <p @click="loadAddress(user.id)">{{ user }}</p>
   </div>
+  <div v-if="addresses.length > 0">
+    <div v-for="address in addresses" :key="address.id">
+        <AddressDetail :address="address"></AddressDetail> 
+    </div> 
+
+    
+    <v-expansion-panels>
+      <v-expansion-panel>
+      <v-expansion-panel-title>
+        {{ user.firstname +user.lastname }}
+      </v-expansion-panel-title>
+        <v-expansion-panels>
+          <v-expansion-panel
+            v-for="address in addresses" :key="address.id"
+            :title="address.street" 
+            :text="address.number"
+          ></v-expansion-panel>
+        </v-expansion-panels>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    
+  
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 
-import axios from 'axios'
+import AddressDetail from './AddressDetail.vue'
 
 export default {
+  components: { AddressDetail },
   props: {
-    user: {}
+    user: {},
+  },
+  data() {
+    return {
+      addresses: [],
+    };
   },
   methods: {
     async loadAddress(userId) {
-      var addresses = await axios.get('http://localhost:3000/addresses?userId=' + userId);
-      addresses.data.forEach( address => console.log(address.street + ' ' + address.number))
-    }
+      var addresses = await axios.get(
+        "http://localhost:3000/addresses?userId=" + userId
+      );
+      addresses.data.forEach((address) =>
+        console.log(address.street + " " + address.number)
+      );
+      this.addresses = addresses.data;
+    },
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
